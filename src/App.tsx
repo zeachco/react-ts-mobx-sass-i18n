2f2d0f9beec8a50e0f28b79243fcc5b6c48cbe58
Olivier <zeachco@gmail.com>
2019-01-15 16:47:33 -0500

    added react-dom-router

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.scss';
 import { app } from './models/App';
 import { l } from './models/I18n';
 import { LanguageSelector } from './components/LanguagesSelector';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 app.i18n.load('fr')
 
@@ -14,19 +15,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p dangerouslySetInnerHTML={{ __html: l('edit_and_reload') }} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {l('app_title')}
-          </a>
-          <LanguageSelector />
-        </header>
+        <BrowserRouter>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p dangerouslySetInnerHTML={{ __html: l('edit_and_reload') }} />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              {l('app_title')}
+            </a>
+            <Route path="/:lang*" component={LanguageSelector} />
+            {/* <LanguageSelector /> */}
+          </header>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/LanguagesSelector.tsx
+++ src/components/LanguagesSelector.tsx
@@ -1,8 +1,11 @@
 import React, { MouseEvent } from 'react';
+import { match as RouterMatch, Redirect } from 'react-router';
+import cx from 'classnames'
 
 import { app } from '../models/App'
 import { ILanguagesCode, l } from '../models/I18n';
 import style from './LanguagesSelector.module.scss';
+import { observer } from 'mobx-react';
 
 const handleLangChange = (lang: ILanguagesCode) => (e: MouseEvent) => {
     e.preventDefault();
@@ -11,18 +14,39 @@ const handleLangChange = (lang: ILanguagesCode) => (e: MouseEvent) => {
 
 interface IProps {
     short?: boolean
+    match: RouterMatch
 }
 
-export const LanguageSelector: React.SFC<IProps> = ({
-    short = false
-}) => (
-    <div className={style.languages}>
-        {
-            app.i18n.supportedLanguages.map((lang) => (
-                <a key={lang} href={"#" + lang} onClick={handleLangChange(lang)} title={l(lang)}>
-                    {l(short ? lang + '_short' : lang)}
-                </a>
-            ))
-        }
-    </div>
-);
+export const LanguageSelector: React.SFC<IProps> = observer(({
+    short = false,
+    match
+}) => {
+    let targetRoot = match.url.replace(/^\/[a-z]{2}\//, `/${app.i18n.language}/`);
+    let doRedirect = match.url !== targetRoot;
+
+    if (!match.params.lang) {
+        targetRoot = `/${app.i18n.language}/`;
+        doRedirect = true;
+    }
+
+    return (
+        <div className={style.languages}>
+            {
+                app.i18n.supportedLanguages.map((lang) => (
+                    <a
+                        key={lang}
+                        href={"#" + lang}
+                        onClick={handleLangChange(lang)}
+                        title={l(lang)}
+                        className={cx({
+                            [style.current]: lang === app.i18n.language
+                        })}
+                    >
+                        {l(short ? lang + '_short' : lang)}
+                    </a>
+                ))
+            }
+            {doRedirect && <Redirect to={targetRoot} />}
+        </div>
+    );
+});
